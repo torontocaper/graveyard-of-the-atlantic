ea0ac337be33ad569564ccab5dc4bd16ef08510f
Adam <torontocaper@gmail.com>
2026-09-22 22:28:34 -0400

    got something going with the shader

--- src/water.gd
+++ src/water.gd
@@ -1,16 +1,26 @@
 class_name Water
 extends MeshInstance3D
 
-@export var displacement_scale : float = 0.1
+@export var x_wave_amplitude : float = 2.0 ## Peak deviation from zero aka maximum height in metres
+@export var x_wave_frequency : float = 2.0 ## Number of waves per second?
+@export var x_wave_speed : float = 0.1 ## Velocity of wave in metres/second
+
+@export var z_wave_amplitude : float = 2.0 ## Peak deviation from zero aka maximum height in metres
+@export var z_wave_frequency : float = 2.0 ## Number of waves per second?
+@export var z_wave_speed : float = 0.1 ## Velocity of wave in metres/second
 
 var water_shader : ShaderMaterial
 
-@onready var wave_manager: WaveManager = $"../WaveManager"
-
 func _ready() -> void:
 	water_shader = get_active_material(0)
-	water_shader.set_shader_parameter("y", displacement_scale)
-	
+	water_shader.set_shader_parameter("x_amplitude", x_wave_amplitude)
+	water_shader.set_shader_parameter("x_frequency", x_wave_frequency)
+	water_shader.set_shader_parameter("x_speed", x_wave_speed)
+	water_shader.set_shader_parameter("z_amplitude", z_wave_amplitude)
+	water_shader.set_shader_parameter("z_frequency", z_wave_frequency)
+	water_shader.set_shader_parameter("z_speed", z_wave_speed)
 
-func _process(delta: float) -> void:
-	water_shader.set_shader_parameter("x", randf())
+func get_wave_height_at_position_1d(position_x : float) -> float:
+	var height : float
+	height = x_wave_amplitude * ( sin( ( 2.0 * PI * x_wave_frequency ) * ( ( position_x + Time.get_ticks_msec() ) * x_wave_speed ) ) )
+	return height

9e2b3b5edd8de326da744dac2edc08884906295d
Adam <torontocaper@gmail.com>
2026-09-25 23:03:32 -0400

    broke it a bit

--- src/rowboat.gd
+++ src/rowboat.gd
@@ -2,6 +2,20 @@ class_name Rowboat
 extends RigidBody3D
 
 @export var row_power : float = 100.0
+@export var water : Water
+
+var markers : Array[Marker3D]
+
+@onready var marker_bow: Marker3D = $MarkerBow
+@onready var marker_starboard_aft: Marker3D = $MarkerStarboardAft
+@onready var marker_port_aft: Marker3D = $MarkerPortAft
+
+func _ready() -> void:
+	markers = [
+		marker_bow,
+		marker_port_aft,
+		marker_starboard_aft
+	]
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("row"):
--- src/water.gd
+++ src/water.gd
@@ -1,33 +1,81 @@
 class_name Water
 extends MeshInstance3D
 
-@export var x_wave_amplitude : float = 0.05 ## Peak deviation from zero aka maximum height in metres
-@export var x_wave_frequency : float = 1.5 ## Number of waves per second?
-@export var x_wave_speed : float = 0.1 ## Velocity of wave in metres/second
+@export var boat : Rowboat
 
-@export var z_wave_amplitude : float = 0.1 ## Peak deviation from zero aka maximum height in metres
-@export var z_wave_frequency : float = 0.6 ## Number of waves per second?
-@export var z_wave_speed : float = 0.35 ## Velocity of wave in metres/second
+@export_group("Waves")
+@export var x_wave : Vector3 = Vector3(
+	0.05,
+	1.5,
+	0.1
+):
+	set(value):
+		x_wave = value
+		set_shader_parameters()
+@export var z_wave : Vector3 = Vector3(
+	0.05,
+	1.5,
+	0.1
+):
+	set(value):
+		z_wave = value
+		set_shader_parameters()
+@export var diagonal_wave : Vector3 = Vector3(
+	0.15,
+	0.1,
+	0.3
+):
+	set(value):
+		diagonal_wave = value
+		set_shader_parameters()
 
-@export var diagonal_wave_amplitude : float = 0.15 ## Peak deviation from zero aka maximum height in metres
-@export var diagonal_wave_frequency : float = 0.1 ## Number of waves per second?
-@export var diagonal_wave_speed : float = 0.3 ## Velocity of wave in metres/second
+#@export var x_wave.x : float = 0.05 ## Peak deviation from zero aka maximum height in metres
+#@export var x_wave.y : float = 1.5 ## Number of waves per second?
+#@export var x_wave.z : float = 0.1 ## Velocity of wave in metres/second
+#
+#@export var z_wave.x : float = 0.1 ## Peak deviation from zero aka maximum height in metres
+#@export var z_wave.y : float = 0.6 ## Number of waves per second?
+#@export var z_wave.z : float = 0.35 ## Velocity of wave in metres/second
+#
+#@export var diagonal_wave.x : float = 0.15 ## Peak deviation from zero aka maximum height in metres
+#@export var diagonal_wave.y : float = 0.1 ## Number of waves per second?
+#@export var diagonal_wave.z : float = 0.3 ## Velocity of wave in metres/second
 
 var water_shader : ShaderMaterial
+var water_body : StaticBody3D
+var water_shape : ConcavePolygonShape3D
 
 func _ready() -> void:
+	create_trimesh_collision()
+	water_body = get_child(0) as StaticBody3D
+	water_shape = water_body.get_child(0).shape
 	water_shader = get_active_material(0)
-	water_shader.set_shader_parameter("x_amplitude", x_wave_amplitude)
-	water_shader.set_shader_parameter("x_frequency", x_wave_frequency)
-	water_shader.set_shader_parameter("x_speed", x_wave_speed)
-	water_shader.set_shader_parameter("z_amplitude", z_wave_amplitude)
-	water_shader.set_shader_parameter("z_frequency", z_wave_frequency)
-	water_shader.set_shader_parameter("z_speed", z_wave_speed)
-	water_shader.set_shader_parameter("diagonal_amplitude", diagonal_wave_amplitude)
-	water_shader.set_shader_parameter("diagonal_frequency", diagonal_wave_frequency)
-	water_shader.set_shader_parameter("diagonal_speed", diagonal_wave_speed)
+	set_shader_parameters()
 
-func get_wave_height_at_position_1d(position_x : float) -> float:
+func _physics_process(_delta: float) -> void:
+	if water_shape:
+		var old_faces : PackedVector3Array = water_shape.get_faces()
+		var new_faces : PackedVector3Array = []
+		for face in old_faces:
+			new_faces.append(Vector3(face.x, get_height_at_position(face.x, face.z), face.z))
+		water_shape.set_faces(new_faces)
+
+func get_height_at_position(position_x : float, position_z : float) -> float:
 	var height : float
-	height = x_wave_amplitude * ( sin( ( 2.0 * PI * x_wave_frequency ) * ( ( position_x + Time.get_ticks_msec() ) * x_wave_speed ) ) )
+	var x_wave_value : float = x_wave.x * sin( (2.0 * PI * x_wave.y) * ( position_x + Time.get_ticks_msec() * x_wave.z  ));
+	var z_wave_value : float = z_wave.x * sin( (2.0 * PI * z_wave.y) * ( position_z + Time.get_ticks_msec() * z_wave.z  ));
+	var diagonal_wave_value : float = diagonal_wave.x * sin( (2.0 * PI * diagonal_wave.y) * ( position_x + position_z +  Time.get_ticks_msec() * diagonal_wave.z ));
+	height = x_wave_value + z_wave_value + diagonal_wave_value;
 	return height
+
+func set_shader_parameters() -> void:
+	if water_shader:
+		water_shader.set_shader_parameter("x_amplitude", x_wave.x)
+		water_shader.set_shader_parameter("x_frequency", x_wave.y)
+		water_shader.set_shader_parameter("x_speed", x_wave.z)
+		water_shader.set_shader_parameter("z_amplitude", z_wave.x)
+		water_shader.set_shader_parameter("z_frequency", z_wave.y)
+		water_shader.set_shader_parameter("z_speed", z_wave.z)
+		water_shader.set_shader_parameter("diagonal_amplitude", diagonal_wave.x)
+		water_shader.set_shader_parameter("diagonal_frequency", diagonal_wave.y)
+		water_shader.set_shader_parameter("diagonal_speed", diagonal_wave.z)
